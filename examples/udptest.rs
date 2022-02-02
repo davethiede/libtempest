6b33adfb19c1f24c30c60df9013aae031d8ecfa5
David Thiede <davidrthiede@gmail.com>
2022-02-02 10:45:44 -0800

    Updated the example program to use the library.
    The example program now uses the clap cli interface to allow
    monitoring udp packets with raw json, serde_json::Value or
    tempest::Tempest outputs.

--- examples/udptest.rs
+++ examples/udptest.rs
@@ -1,27 +1,74 @@
 use serde_json::Value;
 use std::net::UdpSocket;
 
-fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:50222").expect("bind failure");
+use clap::Parser;
 
-    let mut buf = [0; 400];
-    for _ in 1..=10 {
+#[derive(Debug, clap::ArgEnum, Clone, Parser)]
+enum Mode {
+    Struct,
+    Raw,
+    Parsed,
+}
+
+/// Read tempest json weatherflow packets from a network interface
+/// and display the resulting data. The default addr value will
+/// read packets broadcast on the local subnet on port 50222.
+#[derive(Debug, Parser)]
+struct Arg {
+    /// Exit after processing `count` packets.
+    #[clap(long, short, value_name = "#")]
+    count: Option<usize>,
+
+    /// Packet buffer size.
+    ///
+    /// Buffer must be large enough to hold entire tempest UDP packet.
+    #[clap(long, short, value_name = "#", default_value_t = 400)]
+    bufsize: usize,
+
+    /// Listen addr:port
+    #[clap(long, short, default_value = "0.0.0.0:50222")]
+    addr: String,
+
+    /// Display mode.
+    ///
+    /// struct: Parse into `Tempest`.{n}
+    /// parsed: Parse into generic `serde_json::Value`.{n}
+    /// raw   : Display the text obtained from the packet.
+    #[clap(arg_enum, short, long, default_value_t=Mode::Struct)]
+    mode: Mode,
+}
+
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let args = Arg::parse();
+    let mut buf = vec![0_u8; args.bufsize];
+
+    let socket = UdpSocket::bind(args.addr).expect("bind failure");
+    let mut n: usize = 0;
+
+    loop {
         let (amt, src) = socket.recv_from(&mut buf).expect("no data");
-        println!(
-            "recv: {} {} {:?}",
-            amt,
-            src,
-            std::str::from_utf8(&buf[..amt])
-        );
-        println!("before match");
-        match std::str::from_utf8(&buf[..amt]) {
-            Ok(s) => {
+        match &args.mode {
+            Mode::Raw => {
+                println!("recv: {amt} {src} {:?}", std::str::from_utf8(&buf[..amt]));
+            }
+            Mode::Parsed => {
+                let s = std::str::from_utf8(&buf[..amt])?;
                 let v: Value = serde_json::from_str(s)?;
                 println!("decoded json {:?}", &v);
             }
-            Err(e) => println!("error {:?}", e),
+            Mode::Struct => {
+                let v: tempest::Tempest = serde_json::from_slice(&buf[..amt])?;
+                println!("tempest::Tempest = {v:?}");
+            }
         }
-        println!("after match");
+
+        // if count specified, then only process that many packets and stop.
+        if let Some(m) = args.count {
+            n = n + 1;
+            if n >= m {
+                break;
+            }
+        }
     }
     Ok(())
 }
